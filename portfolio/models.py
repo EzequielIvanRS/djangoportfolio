6071b7a477860a0aab2ae3f8b2ecb88180b6a354
EzequielIvanRS <ezequielivan.rs@gmail.com>
2023-07-16 02:04:05 -0300

    proyectowebportafolio

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     image = models.ImageField( verbose_name="Imagen", upload_to="projects")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualizacion")
-
+    link = models.URLField(null=True, blank=True, verbose_name="Link")
     def __str__(self):
         return self.name
 
